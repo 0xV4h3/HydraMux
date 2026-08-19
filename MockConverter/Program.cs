015b418528879278ba9a736e10365892967aee03
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-08-19 15:17:38 +0400

    simple MockConverter done

--- MockConverter/Program.cs
+++ MockConverter/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello from MockConverter!");
+        ulong totalBytes = 300 * 1024 * 1024;
+        ulong bufferSize = 4 * 1024 * 1024;
+        ulong currentPosition = 0;
+
+        Random rand = new Random();
+        
+        Console.WriteLine($"TOTAL:{totalBytes}");
+
+        while (currentPosition < totalBytes)
+        {
+            Thread.Sleep(rand.Next(50, 150));
+            currentPosition += bufferSize;
+            
+            Console.WriteLine($"TICK:{currentPosition}");
+        }
+        
+        Environment.Exit(rand.Next(0, 10) == 0 ? 1 : 0);
     }
 }
