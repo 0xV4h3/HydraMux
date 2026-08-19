89c20f22c9e88d63c3835d8b5fcf96d2b977feba
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-08-19 17:53:48 +0400

    Job working model with snapshot created

---
+++ ConversionManager/Job.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using ConversionManager.Utils;
+
+namespace ConversionManager;
+
+public enum JobStatus
+{
+    Queued, 
+    Running,
+    Completed,
+    Failed,
+    Canceled
+}
+
+public readonly record struct JobSnapshot(
+    int Id,
+    string Input,
+    string Output,
+    JobStatus Status,
+    string ProgressLine);
+
+public class Job
+{
+    public int Id { get; set; }
+    public string Input { get; set; } = "";
+    public string Output { get; set; } = "";
+    public string Options { get; set; } = "";
+    public JobStatus Status { get; set; } = JobStatus.Queued;
+
+    public ConsoleProgressBar? ProgressBar { get; set; }
+    public ulong CurrentTick { get; set; }
+    public ulong TotalTicks { get; set; }
+    public string CustomMessage { get; set; } = "";
+
+    public Process? LiveProcess { get; set; }
+}
