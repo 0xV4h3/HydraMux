c55dab75844bff49aeb8fbe976bb1dc304831bb6
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-08-19 19:06:19 +0400

    JobManager blueprint

---
+++ ConversionManager/JobManager.cs
@@ -0,0 +1,10 @@
+﻿namespace ConversionManager;
+
+public class JobManager(string workerExePath)
+{
+    private readonly string _workerExePath = workerExePath;
+    private readonly List<Job> _jobs = new();
+    private readonly object _lock = new(); // some locking primitive
+
+    //TODO methods
+}
