34a087244641cc01bea7fc4212f99c26747ae648
LusineKarapetyann <lusinekarapetyan013@gmail.com>
2026-08-20 19:42:51 +0400

    Job cancellation add

--- Core/JobManager.cs
+++ Core/JobManager.cs
@@ -1,4 +1,6 @@
-﻿namespace Core;
+﻿using System.Diagnostics;
+
+namespace Core;
 
 public class JobManager(string workerExePath)
 {
@@ -17,17 +19,71 @@ public class JobManager(string workerExePath)
     {
         throw new NotImplementedException();
     }
-    
-    public bool CancelJob(int id)
+
+    public bool CancelJob(int jobId)
     {
-        throw new NotImplementedException();
+        lock (_lock)
+        {
+            var job = _jobs.FirstOrDefault(j => j.Id == jobId);
+            if (job == null)
+                return false;
+
+            return TryCancelJobInternal(job);
+        }
     }
-    
+
     public void CancelAll()
     {
-        throw new NotImplementedException();
+        lock (_lock)
+        {
+            foreach (var job in _jobs)
+            {
+                TryCancelJobInternal(job);
+            }
+
+            Monitor.PulseAll(_lock);
+        }
     }
-    
+
+    private bool TryCancelJobInternal(Job job)
+    {
+        if (job.Status != JobStatus.Queued && job.Status != JobStatus.Running)
+        {
+            return false;
+        }
+
+        job.Status = JobStatus.Canceled;
+        job.CustomMessage = "Canceled.";
+        job.ProgressBar?.Dispose();
+
+        if (job.LiveProcess != null)
+        {
+            TryKillProcess(job.LiveProcess);
+            job.LiveProcess = null;
+        }
+
+        Monitor.PulseAll(_lock);
+
+        return true;
+    }
+
+    private static void TryKillProcess(Process process)
+    {
+        if (process == null)
+            return;
+        try
+        {
+            process.Kill(entireProcessTree: true);
+        }
+        catch (Exception ex) when (ex is InvalidOperationException
+                               || ex is System.ComponentModel.Win32Exception)
+        { }
+        catch (AggregateException aggEx)
+        {
+            aggEx.Handle(inner => inner is InvalidOperationException
+                               || inner is System.ComponentModel.Win32Exception);
+        }
+    }
     private void CancelInternal(Job job)
     {
         throw new NotImplementedException();
